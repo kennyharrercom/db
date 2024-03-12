f1cff384aaef9e158cd5fa7582604cb2337e5083
Kenny Harrer <kenny.harrer@icloud.com>
2024-03-12 14:05:44 -0500

    upload example .env, https server

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-global.__basedir = __dirname;
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs'); 
+const https = require('https'); 
 const path = require('path');
-global.DATAFOLDER = path.join(global.__basedir, 'data');
 
+// global variables
+global.__basedir = __dirname;
+global.DATAFOLDER = path.join(global.__basedir, 'data');
 global.CRUDPATH = '/api/collections/';
 
-const express = require('express');
 const app = express();
 const apirouter = require('./api/');
-app.use(express.json());
 
-app.use(CRUDPATH + '*', apirouter);
+app.use(express.json());
+app.use(global.CRUDPATH + '*', apirouter);
 
 app.get('/', (req, res) => {
     res.status(200).send('yipee');
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+if (process.env.HTTPS === 'true') {
+    const sslOptions = {
+        cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+        key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    };
+
+    https.createServer(sslOptions, app).listen(PORT, () => {
+        console.log(`Server is running on https://localhost:${PORT}`);
+    });
+} else {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
